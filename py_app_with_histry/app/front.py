1c138042f85626902fc1167ca3aa9be6553b3d15
Joseph Kanzaveli <youngyotheb@gmail.com>
2024-02-08 15:10:12 +0200

    test

--- py_app_with_histry/app/front.py
+++ py_app_with_histry/app/front.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, jsonify
 from back import get_api
+import json
+import os
 
 app = Flask(__name__)
 
@@ -22,7 +24,9 @@ def home():
     if request.method == "POST":
         return redirect("city")
 
-    return render_template('home.html', name="Joseph", title="weather-home")
+    BG_COLOR = os.getenv('BG_COLOR')
+
+    return render_template('home.html', name="Joseph", title="weather-home", color=BG_COLOR)
 
 
 @app.route("/city", methods=['GET', 'POST'])
@@ -30,17 +34,23 @@ def user():
     """
         HTML page with weather stat 7 days forward
     """
-    city_input = request.form.get("city_input").capitalize()
-
+    city_input = request.form.get("city_input")
     # Test for empty input
     if city_input == "":
         city_input = "London"
-
     try:
         # Use Back module to get API
         list_days = get_api(city_input)
     except Exception:
         return redirect("error")
+    
+    # Write history
+    data_json = {
+        'city': city_input,
+    }
+    with open('history.json', 'a') as f:
+        json.dump(data_json, f)
+        f.write('\n')
 
     return render_template('city.html',  city=city_input, title=city_input, list=list_days, back="/")
 
@@ -51,6 +61,17 @@ def err():
         Error page
     """
     return render_template("error.html",  title="error-page", back="/")
+
+@app.route("/history")
+def history():
+    """
+        History page
+    """
+    data = ''
+    with open('history.json', 'r') as f:
+        data = f.readlines()
+
+    return render_template("history.html",  title="history-page", back="/", json=data)
 
 
 if __name__ == "__main__":
