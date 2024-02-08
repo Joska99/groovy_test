b3f8abc196103ca300e893269a9618f6e99d96ec
Joseph Kanzaveli <youngyotheb@gmail.com>
2024-02-08 20:03:13 +0200

    ingress

--- k8s_ingress/install_ingress.sh
+++ k8s_ingress/install_ingress.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.2.1/deploy/static/provider/cloud/deploy.yaml
+helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
+helm search repo ingress-nginx --version "*"
+
+CHART_VERSION="4.9.1"
+APP_VERSION="1.9.6"
+
+mkdir ingress_set_up
+
+helm template ingress-nginx ingress-nginx \
+    --repo https://kubernetes.github.io/ingress-nginx \
+    --version ${CHART_VERSION} \
+    --namespace ingress-nginx \
+    > ./ingress_set_up/nginx_ingress.${APP_VERSION}.yaml
+
+kubectl create namespace ingress-nginx
+kubectl apply -f ./ingress_set_up/nginx_ingress.${APP_VERSION}.yaml
+
+kubectl get all -n ingress-nginx
